e1d2e83e3985e1a8824fcb4f79514dbe82347847
MarkBlinder <markblndr@gmail.com>
2022-10-25 22:10:46 -0400

    initial commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Mark Blinder on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

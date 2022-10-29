1cb6b8ca460165190ad514e00de49bc6d7e42403
MarkBlinder <markblndr@gmail.com>
2022-10-29 19:14:34 -0400

    added first cards

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -8,19 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🎃", "👻", "💀", "🧛"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, iOS world!")
+        HStack {
+            CardView(isFaceUp: true, content: emojis[0])
+            CardView(isFaceUp: true, content: emojis[1])
+            CardView(isFaceUp: false, content: emojis[2])
+            CardView(isFaceUp: false, content: emojis[3])
         }
         .padding()
+        .foregroundColor(.red)
     }
 }
+
+struct CardView: View {
+    @State var isFaceUp : Bool = false
+    var content: String
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 25)
+            if isFaceUp {
+                shape.fill(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }
+            else {
+                shape.fill(.red)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
